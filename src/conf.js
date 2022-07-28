778f650ced3766c8593be0e6c12432890eafd9fc
Nickhenry784 <nhiennguyen1605@gmail.com>
2022-07-28 13:15:09 +0700

    ok

---
+++ src/conf.js
@@ -0,0 +1,37 @@
+export const items = [
+  {
+    sku: 'com.amazon.tnamesevs_1',
+    value: 50,
+  },
+  {
+    sku: 'com.amazon.tnamesevs_2',
+    value: 100,
+  },
+  {
+    sku: 'com.amazon.tnamesevs_3',
+    value: 300,
+  },
+  {
+    sku: 'com.amazon.tnamesevs_4',
+    value: 500,
+  },
+];
+
+export const subs = [
+  {
+    sku: 'com.amazon.weekly_tnamesevs_1',
+    value: 100,
+  },
+  {
+    sku: 'com.amazon.weekly_tnamesevs_2',
+    value: 200,
+  },
+  {
+    sku: 'com.amazon.monthly_tnamesevs_1',
+    value: 400,
+  },
+  {
+    sku: 'com.amazon.monthly_tnamesevs_2',
+    value: 800,
+  },
+];
---
+++ src/screens/ItemScreen.js
@@ -0,0 +1,87 @@
+import { 
+  StyleSheet, 
+  TouchableOpacity, Dimensions, 
+  ScrollView,
+  Image,
+  ImageBackground, 
+  Alert,  
+  View} from "react-native";
+import React, {useEffect, useState} from 'react';
+import { images } from "../assets";
+
+const windowWidth = Dimensions.get('screen').width;
+const windowHeight = Dimensions.get('screen').height;
+
+const ItemScreen = ({navigation, route}) => {
+  const {background} = route.params;
+  const [index, setIndex] = useState(0);
+
+
+  const onClickNextBtn = () => {
+    if(index !== background.length - 1){
+      setIndex(index + 1);
+    }
+  }
+
+  const onClickBackBtn = () => {
+    if(index !== 0){
+      setIndex(index - 1);
+    }
+  }
+
+
+  return (
+      <ScrollView>
+        <ImageBackground source={background[index]} style={appStyle.scrollStyle}>
+        <View style={appStyle.bottomView}>
+          <TouchableOpacity onPress={() => onClickBackBtn()}>
+            <Image source={images.back} style={appStyle.btn} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Image source={images.home} style={appStyle.btn} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => onClickNextBtn()}>
+            <Image source={images.next} style={appStyle.btn} />
+          </TouchableOpacity>
+      </View>
+        </ImageBackground>
+      </ScrollView>
+  );
+};
+
+
+export const appStyle = StyleSheet.create({
+  homeView: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    resizeMode: 'cover',
+  },
+  scrollStyle: {
+    width: windowWidth,
+    height: windowHeight * 1.2,
+    resizeMode: 'contain',
+    justifyContent: 'flex-end',
+  },
+  startBtn: {
+    width: windowWidth * 0.3,
+    height: windowHeight * 0.1,
+    resizeMode: 'contain',
+  },
+  btn: {
+    width: windowWidth * 0.3,
+    height: windowHeight * 0.1,
+    resizeMode: 'contain',
+  },
+  bottomView: {
+    width: windowWidth,
+    height: windowHeight * 0.2,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+});
+
+export default ItemScreen;
